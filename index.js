e0666565f79ee1781f60e16bb5f888b36d86c626
Peter Heywood <peter.heywood@ombudsman.org.uk>
2024-09-13 15:26:38 +0100

    Finished code for converting CSV

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-let csvToJson = require("convert-csv-to-json");
-let express = require("express");
-
-let json = csvToJson.getJsonFromCsv("assets.csv");
-let jsonObject = [];
+const csvToJson = require("convert-csv-to-json");
+const express = require("express");
+const fs = require("fs/promises");
 
 let app = express();
 let port = 3000;
 
-app.post("/convert", (req, res) => {
+app.post("/convert", async (req, res) => {
   console.log("New convert request received");
-  for (let i = 0; i < json.length; i++) {
-    let tag = Object.values(json[i])[0].split(",")[0].replaceAll('"', "");
-    let name = Object.values(json[i])[0].split(",")[1].replaceAll('"', "");
-    let status = Object.values(json[i])[0].split(",")[6].replaceAll('"', "");
-    let substate = Object.values(json[i])[0].split(",")[7].replaceAll('"', "");
-    let serial = Object.values(json[i])[0].split(",")[5].replaceAll('"', "");
-    jsonObject.push({
-      name: name,
-      status: status == 2 ? "archived" : "active",
-      substate: substate,
-      tag: tag,
-      serial: serial,
+  try {
+    let data = [];
+    req.on("data", (chunk) => {
+      data.push(chunk);
     });
+
+    req.on("end", async () => {
+      if (data.length === 0) {
+        res.status(400).send("No file or file is blank");
+      } else {
+        console.log("Writing temp file for conversion");
+        await fs.writeFile("data.csv", Buffer.concat(data), "base64", (e) => {
+          if (e) {
+            res.status(400).send(e);
+          }
+        });
+        let json = csvToJson.fieldDelimiter(",").getJsonFromCsv("data.csv");
+        jsonString = JSON.stringify(json).replaceAll('\\"', "");
+        console.log();
+        await fs.rm("data.csv");
+        res.send(JSON.parse(jsonString));
+      }
+    });
+  } catch (e) {
+    res.status(400).send(e);
   }
-  res.send(jsonObject);
 });
 
 app.listen(port ?? 3000, () => {
